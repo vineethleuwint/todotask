e59a8ccc6f94dc1f0d8af41a491e60be6f41ddee
rahular555 <123058366+rahular555@users.noreply.github.com>
2023-02-07 15:37:50 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainViewComponent } from './pages/main-view/main-view.component';
+import { MainViewComponent,DialogOverviewExampleDialog } from './pages/main-view/main-view.component';
 import { FormsModule } from '@angular/forms'
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import { DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainViewComponent, 
+    MainViewComponent,
+    DialogOverviewExampleDialog,
+ 
   ],
   imports: [
     BrowserModule,
@@ -21,7 +28,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     DragDropModule,
     DateTimePickerModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
