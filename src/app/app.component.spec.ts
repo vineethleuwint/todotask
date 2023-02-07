d79cc4138198655d2eaca3f7f60854719cfdc16d
ArshaSreenivasan <aarsha.aarshu@gmail.com>
2023-02-07 13:22:58 +0530

    login page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angloginpage'`, () => {
+  it(`should have as title 'anglog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angloginpage');
+    expect(app.title).toEqual('anglog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angloginpage app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('anglog app is running!');
   });
 });
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/internal/Subscription';
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit{
+  public signupForm!: FormGroup;
+
+
+
+constructor(private formBuilder:FormBuilder, private http :HttpClient,private router:Router){
+
+}
+
+
+  ngOnInit(): void{
+this.signupForm=this.formBuilder.group({
+  fullname:[''],
+  email:[''],
+  password:['']
+})
+  }
+  signup(){
+    this.http.post<any>("http://localhost:3000/user",this.signupForm.value)
+    .subscribe(res=>{
+      alert("signup successfull");
+      this.signupForm.reset();
+      this.router.navigate(['login']);
+    },err=>{
+      alert("something went wrong")
+  })
+  }}
