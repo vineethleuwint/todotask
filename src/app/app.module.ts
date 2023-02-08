71138a7bd8af96b19a21e62f962509d53d474055
rahular555 <123058366+rahular555@users.noreply.github.com>
2023-02-08 13:16:39 +0530

    Updated with adding names through dialog

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainViewComponent,DialogOverviewExampleDialog } from './pages/main-view/main-view.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDialogModule} from '@angular/material/dialog';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainViewComponent,
+    DialogOverviewExampleDialog,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DragDropModule,
+    DateTimePickerModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
